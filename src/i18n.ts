cc73d1a024f2ddaa0aa37095520b3038ca677939
Adriano Raiano <adriano@raiano.ch>
2022-10-24 10:01:33 +0200

    issue with lazy loading translations

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,17 +1,8 @@
 import i18next from "i18next";
 import { initReactI18next } from "react-i18next";
+import Backend from 'i18next-http-backend';
 
-export const resources = {
-  en: {
-    translation: {
-      key1: {
-        key2: "",
-      },
-    },
-  },
-} as const;
-
-i18next.use(initReactI18next).init({
-  lng: "en",
-  resources,
+i18next.use(Backend).use(initReactI18next).init({
+  debug: true,
+  fallbackLng: "en"
 });
